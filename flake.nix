3024e9ff170da7fde228a735ef5c918d5f7969eb
Mattias Matthiesen <mattmatthi@hotmail.com>
2024-09-11 17:28:27 +0200

    brew: Cask list update

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  description = "nix-darwin configuration for monochromatti";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-homebrew = {
+      url = "github:zhaofengli-wip/nix-homebrew";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+    homebrew-bundle = {
+      url = "github:homebrew/homebrew-bundle";
+      flake = false;
+    };
+  };
+
+
+  outputs =
+    inputs@{ self
+    , nixpkgs
+    , nix-darwin
+    , home-manager
+    , nix-homebrew
+    , homebrew-cask
+    , homebrew-bundle
+    , ...
+    }: {
+      darwinConfigurations = {
+        "monochromatti-mbp" = nix-darwin.lib.darwinSystem {
+          system = "aarch64-darwin";
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./modules/system.nix
+            home-manager.darwinModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.monochromatti = import ./modules/home.nix;
+            }
+            nix-homebrew.darwinModules.nix-homebrew
+            {
+              nix-homebrew = {
+                enable = true;
+                enableRosetta = true;
+                user = "monochromatti";
+                taps = {
+                  "homebrew/homebrew-cask" = homebrew-cask;
+                  "homebrew/homebrew-bundle" = homebrew-bundle;
+                };
+                mutableTaps = false;
+              };
+            }
+          ];
+        };
+      };
+    };
+
+}
---
+++ modules/home.nix
@@ -0,0 +1,107 @@
+{ pkgs, config, lib, ... }:
+let
+  latex = pkgs.texliveMedium.withPackages (ps: with ps; [ arara ]);
+  python = pkgs.python312.withPackages (ps: with ps; [ numpy scipy polars ]);
+  tex-fmt = pkgs.callPackage
+    (pkgs.fetchFromGitHub {
+      owner = "wgunderwood";
+      repo = "tex-fmt";
+      rev = "master";
+      sha256 = "sha256-2HUSEK5s7WkBvbLw/u6RxS7fNA4q6iaNfdnyBqc7d68=";
+    })
+    { };
+in
+{
+  home = {
+
+    stateVersion = "24.05";
+
+    packages = with pkgs; [
+
+      # Terminal
+      tree
+      lazygit
+
+      # Database
+      postgresql_15
+
+      # Nix dev
+      nixfmt-rfc-style # Formatter
+      nixpkgs-fmt # Formatter
+      nil # Language server
+
+      # Rust
+      cargo
+
+      # Python
+      python
+      ruff
+      ruff-lsp
+      uv
+
+      # Dev
+      gh
+      git
+      azure-cli
+
+      # Docs
+      pandoc
+      quarto
+      latex
+      tex-fmt
+
+      # Utils
+      syncthing
+      soundsource
+    ];
+  };
+
+  programs = {
+
+    alacritty = {
+      enable = true;
+      settings = {
+        shell.program = "zsh";
+      };
+    };
+
+    zsh = {
+      enable = true;
+      autocd = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+      plugins = [
+        {
+          name = "powerlevel10k-config";
+          src = lib.cleanSource ../dotfiles;
+          file = "p10k.zsh";
+        }
+      ];
+      zplug = {
+        enable = true;
+        plugins = [
+          { name = "zsh-users/zsh-autosuggestions"; }
+          { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; }
+        ];
+      };
+      envExtra = ''
+        zstyle ':autocomplete:*' list-lines 5
+      '';
+    };
+
+    zoxide = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    direnv = {
+      enable = true;
+      enableZshIntegration = true;
+      nix-direnv.enable = true;
+    };
+
+    dircolors.enable = true;
+    fzf.enable = true;
+  };
+
+}
