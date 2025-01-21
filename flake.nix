a3978c3eca306c0654a7a3483cfaa64962e5b97b
Mattias Matthiesen <mattmatthi@hotmail.com>
2025-01-21 21:21:33 +0100

    flake: Restructure

--- flake.nix
+++ flake.nix
@@ -2,17 +2,20 @@
   description = "Nix-darwin configuration for macarius/monochromatti";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
+    };
     nix-darwin = {
-      url = "github:LnL7/nix-darwin";
+      url = "github:LnL7/nix-darwin/nix-darwin-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    nix-homebrew = {
+      url = "github:zhaofengli-wip/nix-homebrew";
+    };
     homebrew-core = {
       url = "github:homebrew/homebrew-core";
       flake = false;
@@ -25,55 +28,30 @@
       url = "github:homebrew/homebrew-bundle";
       flake = false;
     };
-    agenix.url = "github:ryantm/agenix";
+    agenix = {
+      url = "github:ryantm/agenix";
+    };
   };
 
   outputs =
-    inputs@{ self
-    , nixpkgs
-    , nixpkgs-unstable
-    , home-manager
-    , nix-darwin
-    , nix-homebrew
-    , homebrew-core
-    , homebrew-cask
-    , homebrew-bundle
-    , agenix
-    , ...
+    inputs@{
+      self,
+      nixpkgs,
+      home-manager,
+      nix-darwin,
+      nix-homebrew,
+      homebrew-core,
+      homebrew-cask,
+      homebrew-bundle,
+      agenix,
+      ...
     }:
     let
       system = "aarch64-darwin";
-      unstable = import nixpkgs-unstable { inherit system; };
       users = {
         monochromatti = {
           description = "Mattias Matthiesen";
           home = "/Users/monochromatti";
-        };
-      };
-      declarativeHome = {
-        config = {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users."monochromatti" = ./home.nix;
-        };
-      };
-      overlayModule = {
-        nixpkgs.overlays = [
-          (final: prev: { uv = unstable.pkgs.uv; }) # Most recent `uv`
-        ];
-      };
-      nixHomebrew = { ... }: {
-        nix-homebrew = {
-          enable = true;
-          enableRosetta = true;
-          user = "monochromatti";
-          taps = {
-            "homebrew/homebrew-core" = homebrew-core;
-            "homebrew/homebrew-cask" = homebrew-cask;
-            "homebrew/bundle" = homebrew-bundle;
-          };
-          mutableTaps = false;
-          autoMigrate = true;
         };
       };
     in
@@ -85,11 +63,14 @@
           };
           modules = [
             ./darwin.nix
-            home-manager.darwinModules.home-manager
+            ./homebrew.nix
             nix-homebrew.darwinModules.nix-homebrew
-            nixHomebrew
-            declarativeHome
-            overlayModule
+            home-manager.darwinModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users."monochromatti" = import ./home.nix;
+            }
           ];
         };
       };
