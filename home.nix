472f7042f3a2784434ddd8b23001aa6c6cc6fc83
Mattias Matthiesen <mattmatthi@hotmail.com>
2025-02-01 22:01:08 +0100

    home: zed-editor

--- home.nix
+++ home.nix
@@ -1,7 +1,18 @@
-{ pkgs, config, lib, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 let
   latex = pkgs.texliveMedium.withPackages (ps: with ps; [ arara ]);
-  python = pkgs.python312.withPackages (ps: with ps; [ numpy scipy polars ]);
+  python = pkgs.python312.withPackages (
+    ps: with ps; [
+      numpy
+      scipy
+      polars
+    ]
+  );
 in
 {
   home = {
@@ -20,6 +31,10 @@ in
       nixpkgs-fmt # Formatter
       nil # Language server
 
+      # Rust dev
+      rust-analyzer
+      cargo
+
       # Dev
       gh
       git
@@ -35,12 +50,6 @@ in
       # Utils
       age
     ];
-  };
-
-  services = {
-    syncthing = {
-      enable = true;
-    };
   };
 
   programs = {
@@ -63,7 +72,13 @@ in
         enable = true;
         plugins = [
           { name = "zsh-users/zsh-autosuggestions"; }
-          { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; }
+          {
+            name = "romkatv/powerlevel10k";
+            tags = [
+              "as:theme"
+              "depth:1"
+            ];
+          }
         ];
       };
       envExtra = ''
@@ -80,6 +95,114 @@ in
       enable = true;
       enableZshIntegration = true;
       nix-direnv.enable = true;
+    };
+
+    zed-editor = {
+      enable = true;
+      extensions = [
+        "nix"
+        "toml"
+        "json"
+      ];
+
+      ## everything inside of these brackets are Zed options.
+      userSettings = {
+
+        assistant = {
+          enabled = true;
+          version = "2";
+          default_open_ai_model = null;
+          default_model = {
+            provider = "zed.dev";
+            model = "claude-3-5-sonnet-latest";
+          };
+        };
+
+        node = {
+          path = lib.getExe pkgs.nodejs;
+          npm_path = lib.getExe' pkgs.nodejs "npm";
+        };
+
+        hour_format = "hour24";
+        auto_update = false;
+        terminal = {
+          alternate_scroll = "off";
+          blinking = "off";
+          copy_on_select = false;
+          dock = "bottom";
+          detect_venv = {
+            on = {
+              directories = [
+                ".env"
+                "env"
+                ".venv"
+                "venv"
+              ];
+              activate_script = "default";
+            };
+          };
+          env = {
+            TERM = "ghostty";
+          };
+          font_family = "FiraCode Nerd Font";
+          font_features = null;
+          font_size = null;
+          line_height = "comfortable";
+          option_as_meta = false;
+          button = false;
+          shell = "system";
+          toolbar = {
+            title = true;
+          };
+          working_directory = "current_project_directory";
+        };
+
+        lsp = {
+          rust-analyzer = {
+            binary = {
+              path = lib.getExe pkgs.rust-analyzer;
+              path_lookup = true;
+            };
+          };
+          nix = {
+            binary = {
+              path_lookup = true;
+            };
+          };
+        };
+
+        # languages = {
+        #   python = {
+        #     formatter = {
+        #       provider = "ruff";
+        #       binary = {
+        #         path = lib.getExe python;
+        #         path_lookup = true;
+        #       };
+        #     };
+        #     linter = {
+        #       provider = "ruff";
+        #       binary = {
+        #         path = lib.getExe python;
+        #         path_lookup = true;
+        #       };
+        #     };
+        #   };
+        # };
+
+        vim_mode = true;
+        load_direnv = "shell_hook";
+        base_keymap = "VSCode";
+        theme = {
+          mode = "system";
+          light = "One Light";
+          dark = "One Dark";
+        };
+        show_whitespaces = "all";
+        # ui_font_size = 16;
+        # buffer_font_size = 16;
+      };
+
     };
 
     dircolors.enable = true;
