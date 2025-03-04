69cdfa3dd26f3ffbfcff594cbbf3bb03220d746f
Mattias Matthiesen <mattmatthi@hotmail.com>
2025-03-04 19:02:54 +0100

    darwin: Replace yabai with loop

--- darwin.nix
+++ darwin.nix
@@ -1,8 +1,9 @@
-{ pkgs, config, users, ... }:
-let
-  brew = import ./dotfiles/brew.nix;
-  main-user = "monochromatti";
-in
+{
+  pkgs,
+  config,
+  users,
+  ...
+}:
 {
   system = {
     stateVersion = 5;
@@ -16,36 +17,11 @@ in
   };
 
   environment.systemPackages = with pkgs; [
-    yabai # Tiling window management
     mas # Mac App Store CLI
   ];
 
   services = {
     nix-daemon.enable = true;
-    yabai = {
-      enable = true;
-      config = {
-        window_placement = "second_child";
-        window_border = "on";
-        window_border_placement = "inset";
-        active_window_border_topmost = "off";
-        window_topmost = "off";
-        window_shadow = "float";
-        active_window_border_color = "0xff5c7e81";
-        normal_window_border_color = "0xff505050";
-        insert_window_border_color = "0xffd75f5f";
-        active_window_opacity = "1.0";
-        normal_window_opacity = "0.5";
-        split_ratio = "0.50";
-        auto_balance = "on";
-        layout = "bsp";
-        top_padding = 5;
-        bottom_padding = 5;
-        left_padding = 5;
-        right_padding = 5;
-        window_gap = 5;
-      };
-    };
   };
 
   security = {
@@ -58,7 +34,10 @@ in
     optimise.automatic = true;
     settings = {
       auto-optimise-store = false;
-      experimental-features = [ "nix-command" "flakes" ];
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
     };
   };
 
@@ -85,6 +64,7 @@ in
       "visual-studio-code"
       "coteditor"
       "ghostty"
+      "loop"
     ];
     masApps = {
       "Pixelmator Pro" = 1289583905;
