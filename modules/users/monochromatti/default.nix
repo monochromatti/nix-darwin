db933f47a74e6bf2d04361bb09f49adcc2cb0fa0
Mattias Matthiesen <mattias.matthiesen@eviny.no>
2026-01-22 19:46:17 +0100

    monochromatti: VM stop aliases

--- modules/users/monochromatti/default.nix
+++ modules/users/monochromatti/default.nix
@@ -36,8 +36,14 @@ in
             start-vidar-prod = ''
               az vm start --subscription 584a2d66-5adc-45d5-b796-9d69d54154d6 --resource-group asgard --name vidar
             '';
+            stop-vidar-prod = ''
+              az vm deallocate --subscription 584a2d66-5adc-45d5-b796-9d69d54154d6 --resource-group asgard --name vidar
+            '';
             start-vidar-dev = ''
               az vm start --subscription 5111c8c6-28f3-4b11-a07f-0aef3ed4721d --resource-group asgard --name vidar
+            '';
+            stop-vidar-dev = ''
+              az vm deallocate --subscription 5111c8c6-28f3-4b11-a07f-0aef3ed4721d --resource-group asgard --name vidar
             '';
           };
         };
