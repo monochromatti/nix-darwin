97c06fac7b55facc52d5933efa50aac4cd9c7fdd
Mattias Matthiesen <mattmatthi@hotmail.com>
2026-01-27 18:43:18 +0100

    home: agentenv alias

--- modules/home-manager/aliases.nix
+++ modules/home-manager/aliases.nix
@@ -4,6 +4,7 @@
     home.shellAliases = {
       lg = "lazygit";
       zed = "zeditor .";
+      agentenv = "nix shell github:numtide/llm-agents.nix#amp github:numtide/llm-agents.nix#tuicr github:fornybar/pydeptree";
     };
   };
 }
